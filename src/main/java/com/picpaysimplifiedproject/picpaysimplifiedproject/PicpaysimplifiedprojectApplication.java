6c52fb8265c1f3573dde710169596b0c0d34a0c3
Darlan Souza <darlansousa.ds@gmail.com>
2023-08-21 23:28:45 -0300

    feat: initial commit

---
+++ src/main/java/com/picpaysimplifiedproject/picpaysimplifiedproject/PicpaysimplifiedprojectApplication.java
@@ -0,0 +1,13 @@
+package com.picpaysimplifiedproject.picpaysimplifiedproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaysimplifiedprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaysimplifiedprojectApplication.class, args);
+	}
+
+}
